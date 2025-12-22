fde84ec3bfa765006c483b798c1f846f5bb0cf79
HaomingMei <haomingmei9@gmail.com>
2025-12-22 17:36:58 -0600

    Continue work tomorrow, need to inform electrical team that tim3 channel 4 is unusable and explain why UP causes double trigger & why it is bad

--- Tail Light Control Board/Core/Src/stm32f1xx_hal_msp.c
+++ Tail Light Control Board/Core/Src/stm32f1xx_hal_msp.c
@@ -202,7 +202,7 @@ void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_pwm)
     hdma_tim3_ch3.Init.PeriphInc = DMA_PINC_DISABLE;
     hdma_tim3_ch3.Init.MemInc = DMA_MINC_ENABLE;
     hdma_tim3_ch3.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    hdma_tim3_ch3.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+    hdma_tim3_ch3.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
     hdma_tim3_ch3.Init.Mode = DMA_NORMAL;
     hdma_tim3_ch3.Init.Priority = DMA_PRIORITY_VERY_HIGH;
     if (HAL_DMA_Init(&hdma_tim3_ch3) != HAL_OK)
